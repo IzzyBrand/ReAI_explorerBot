1289251ebee8f538da05d6134599aee935db706b
izzybrand <isaiah_brand@brown.edu>
2018-04-30 21:45:00 +0000

    Got hub and requests working together

--- request.py
+++ request.py
@@ -2,9 +2,9 @@ import requests
 import cPickle as pickle
 import numpy as np
 
-REQUEST_URL = 'http://localhost:5000/action'
+REQUEST_URL = 'http://138.16.161.77:5000'
 
-def request_action(cam_img, flow_img, lidar_data, motor_speeds):
+def request_action(url, cam_img, flow_img, lidar_data, motor_speeds):
     info_dict = {
         'cam': cam_img,
         'flow': flow_img,
@@ -13,7 +13,7 @@ def request_action(cam_img, flow_img, lidar_data, motor_speeds):
     }
     encoded = pickle.dumps(info_dict)
     headers = {'content-type': 'text'}
-    response = requests.post(REQUEST_URL, data=encoded, headers=headers)
+    response = requests.post(url + "/action", data=encoded, headers=headers)
     return pickle.loads(response.content)
 
 if __name__ == '__main__':
@@ -21,4 +21,4 @@ if __name__ == '__main__':
     flow = np.random.random((100, 100, 3))
     lidar = np.random.random((4,))
     motor = np.random.random((2,))
-    print request_action(cam, flow, lidar, motor)
+    print request_action(REQUEST_URL, cam, flow, lidar, motor)
